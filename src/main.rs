ca6bd9d172b9837213387cdc42099e7515a1dc24
Marek Jaczewski <jaczewski147@gmail.com>
2021-06-06 21:39:25 +0200

    draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::env;
+
+struct Room {
+    storagePlaces: Vec<StoragePlace>,
+}
+
+struct StoragePlace {
+    name: String,
+    description: Option<String>,
+    capacity: u32,
+    items: Vec<Item>,
+}
+
+struct Item {
+    name: String,
+    description: Option<String>,
+    storagePlace: String,
+    importance: String,
+    status: String,
+}
+
+enum Command {
+    items,
+    rooms,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments: Vec<String> = env:args().collect();
+    let command = arguments[1].clone();
+
+    // testing data:
+    let room = Room{storagePlaces: Vec::new()};
+
+    let bookshelf = StoragePlace {
+        name: "bookshelf",
+        capacity: 20,
+        items: Vec::new(),
+        description: None,
+    };
+
+    let item = Item {
+        name: "cv",
+        description: None,
+        importance: "low",
+        status: "New",
+        storagePlace: bookshelf,
+    };
+
+
 }

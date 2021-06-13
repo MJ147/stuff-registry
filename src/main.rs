85ec9c325789cd228460569b2a696799b9688dc7
Marek Jaczewski <jaczewski147@gmail.com>
2021-06-13 20:11:40 +0200

    add rooms, add items, print

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,62 @@
 use std::env;
 
 struct Room {
-    storagePlaces: Vec<StoragePlace>,
+    storage_places: Vec<StoragePlace>,
+}
+
+impl Room {
+    fn add_storage_place(&mut self, name: String) {
+        let storage_place = StoragePlace {name, items: Vec::new()};
+        self.storage_places.push(storage_place);
+    }
+
+    fn addItem(&mut self, item_name: String, storage_name: String) {
+        let item = Item{name: item_name};
+        let storage_place = self.storage_places.iter().find(|&s| s.name.to_string() == storage_name);
+        match storage_place {
+            Some (x) => x.items.push(item),
+        }
+
+    }
+
+    fn print_items(&self) {
+        for storage_place in self.storage_places.iter() {
+            for item in storage_place.items.iter() {
+                println!("{} ({})", item.name, storage_place.name);
+            }
+        }
+    }
 }
 
 struct StoragePlace {
     name: String,
-    description: Option<String>,
-    capacity: u32,
     items: Vec<Item>,
 }
 
 struct Item {
     name: String,
-    description: Option<String>,
-    storagePlace: String,
-    importance: String,
-    status: String,
 }
 
 enum Command {
-    items,
-    rooms,
+    Items,
+    AddRoom(String),
 }
 
 fn main() {
-    let arguments: Vec<String> = env:args().collect();
-    let command = arguments[1].clone();
+    let mut room = Room{storage_places: Vec::new()};
+    room.add_storage_place("living room".to_string());
 
-    // testing data:
-    let room = Room{storagePlaces: Vec::new()};
-
-    let bookshelf = StoragePlace {
-        name: "bookshelf",
-        capacity: 20,
-        items: Vec::new(),
-        description: None,
+    let arguments: Vec<String> = env::args().collect();
+    let command = match arguments[1].as_str() {
+        "items" => Command::Items,
+        "add" => Command::AddRoom(arguments[2].clone()),
+        _ => panic!("Wrong command"),
     };
 
-    let item = Item {
-        name: "cv",
-        description: None,
-        importance: "low",
-        status: "New",
-        storagePlace: bookshelf,
-    };
-
-
+    match command {
+        Command::Items => room.print_items(),
+        Command::AddRoom(room_name) => {
+            room.add_storage_place(room_name);
+        }
+    }
 }
